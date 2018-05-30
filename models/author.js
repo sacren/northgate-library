c9e82bea7d96da8ff71216da4ef892c62ba0ac3b
Jεan Sacren <sakiwit@gmail.com>
2018-05-30 01:11:39 -0600

    Add fields for Author schema definition

--- models/author.js
+++ models/author.js
@@ -4,6 +4,10 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Author = new Schema({
+  firstname: {type: String, required: true, max: 100},
+  lastname: {type: String, required: true, max: 100},
+  birthdate: {type: Date},
+  deathdate: {type: Date}
 })
 
 // export the model

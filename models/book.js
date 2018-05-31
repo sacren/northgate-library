93a98a865fab6930b2fea3ad8a35d7661ef0bcf7
Jεan Sacren <sakiwit@gmail.com>
2018-05-31 01:13:27 -0600

    Add schema definition of Book model

--- models/book.js
+++ models/book.js
@@ -4,6 +4,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Book = new Schema({
+  title: {type: String, required: true},
+  author: {type: Schema.ObjectId, ref: 'Author', required: true},
+  summary: {type: String, required: true},
+  isbn: {type: String, required: true},
+  genre: [{type: Schema.ObjectId, ref: 'Genre'}]
 })
 
 // export the model

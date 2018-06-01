e976a6dfa2254845bd621a940c7b2b49309a57cc
Jεan Sacren <sakiwit@gmail.com>
2018-06-01 02:16:17 -0600

    Add schema definition for Genre model
    1) The model uses name as an identifier to describe the genre.
    2) The name is of type String.
    3) The name is required.
    4) The name has between 3 and 100 characters.

--- models/genre.js
+++ models/genre.js
@@ -4,6 +4,12 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Genre = new Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 100
+  }
 })
 
 // export the model

7d688db2372beb0dec6d8507dd7107ac9dca5763
Jεan Sacren <sakiwit@gmail.com>
2018-06-03 03:34:22 -0600

    Add dummy callbacks in BookInstance controller for routing

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,1 +1,41 @@
 const Bookinstance = require('../models/bookinstance')
+
+// show the list of all BookInstances
+exports.bookinstanceList = function (req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance list')
+}
+
+// show detailed page for specific BookInstance
+exports.bookinstanceDetail = function (req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id)
+}
+
+// show BookInstance create form on GET
+exports.bookinstanceCreateGet = function (req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance create GET')
+}
+
+// handle BookInstance create on POST
+exports.bookinstanceCreatePost = function (req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance create POST')
+}
+
+// show BookInstance delete form on GET
+exports.bookinstanceDeleteGet = function (req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance delete GET')
+}
+
+// handle BookInstance delete on POST
+exports.bookinstanceDeletePost = function (req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance delete POST')
+}
+
+// show BookInstance update form on GET
+exports.bookinstanceUpdateGet = function (req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance update GET')
+}
+
+// handle BookInstance update on POST
+exports.bookinstanceUpdatePost = function (req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance update POST')
+}
